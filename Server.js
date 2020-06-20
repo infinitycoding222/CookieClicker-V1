3dba6f3220cba162038092632989a86cab91389c
infinitycoding222 <arog0660@gmail.com>
2020-06-20 13:09:17 +0300

    Fix bug and added upgrades

--- Server.js
+++ Server.js
@@ -3,39 +3,33 @@ let CookieClicker1 = document.getElementById("Cookie"),
     button2 = document.getElementById("x2");
 let canibuyx2 = false,
     DoIhaveX2 = false;
-let multi = 1;
 // window.onload = (event) => {
 //     window.prompt("On refresh it will reset upgrades and cookies", "")
 // }
-if (parseInt(CookieClicker1.innerHTML) >= 30) {
-    // canibuyx2 = true;
-    console.log("dsadsa")
-};
 
 function onClickCookies() {
     let previous = parseInt(CookieClicker1.innerHTML)
-    CookieClicker1.innerHTML = previous +
-        multi;
-    console.log(".dsafsad")
-};
+    CookieClicker1.innerHTML = previous + 1;
+    console.log("piza")
+}
 
 function onClickCookiesX2() {
     let pr = parseInt(CookieClicker1.innerHTML);
-    multi = 2;
-    CookieClicker1.innerHTML = pr + multi;
+    CookieClicker1.innerHTML = pr + 2;
     console.log("sadasdsa")
 }
 
 function onUpgradeBuyClick() {
-    // DoIhaveX2 = true;
     let pred = parseInt(CookieClicker1.innerHTML);
     CookieClicker1.innerHTML = pred - 30;
     console.log("im dasdsa")
-};
-if (canibuyx2) {
-    button2.onclick = onUpgradeBuyClick;
-};
-button.onclick = onClickCookies;
-if (DoIhaveX2 == true) {
+    DoIhaveX2 = true;
+}
+// if (canibuyx2 == false) {
+//     console.warn("I can\'t buy more upgrades")
+// }
+button2.onclick = onUpgradeBuyClick;
+if (DoIhaveX2 == true)
     button.onclick = onClickCookiesX2;
-};
+if (DoIhaveX2 == false)
+    button.onclick = onClickCookies;
